0968e9bc96d9a1d571a4048dc8b846fe2bbb0498
Chaya Venkatasubbarao <Addy@Addys-MacBook-Air.local>
2017-10-03 14:59:25 -0400

    First Commit

--- CYOA/main.cpp
+++ CYOA/main.cpp
@@ -5,11 +5,47 @@
 //  Created by Chaya Venkatasubbarao on 10/3/17.
 //  Copyright © 2017 Addy Chandrashekar. All rights reserved.
 //
-
+int health;
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(void) {
+    bool startMenu = true;
+    string name;
+    string location = "";
+    int health;
+    string userChoice = "";
+    int hasEscaped = 0;
+    
+    //To see if they want to play the game or not
+    while(startMenu == true) {
+        cout << "Hello and welcome to Addy's Game. Would you like to start the game? Either put yes or no.\n";
+        cin >> userChoice;
+    }
+    
+        if(userChoice == "Yes" || userChoice == "YEs" || userChoice == "YES" || userChoice == "yEs" || userChoice == "yES" || userChoice == "yeS") {
+            cout << "What is your name?";
+            cin >> name;
+        
+        }
+    
+        else if (userChoice == "No" || userChoice == "NO" || userChoice == "no") {
+            cout << "Say yes whenever you want to play the game.";
+    }
+    
+        else {
+        
+            cout << "Please answer with a yes or no.\n";
+            cin >> userChoice;
+    }
+ 
+    
+    if (location == "Start") {
+        cout << "You find yourself in a dark room. You cannot see anything. Would you like to turn on the lights?"
+        cin >> userChoice;
+        if
+    }
+    
+    
+    
 }
